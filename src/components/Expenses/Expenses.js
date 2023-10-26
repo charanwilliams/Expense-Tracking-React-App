1ee6c9ffa2b186c761636c92b194927afa4cc3f5
charanwilliams <109322104+charanwilliams@users.noreply.github.com>
2023-10-25 21:53:40 -0500

    Refactoring

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -5,7 +5,7 @@ import ExpenseFilter from "../NewExpense/ExpenseFilter";
 
 const Expenses = (props) => {
   const [filterYear, setFilterYear] = useState("");
-  const expenses = props.expenses;
+
   const filterHandler = (filterYear) => {
     setFilterYear(filterYear);
   };
@@ -20,25 +20,30 @@ const Expenses = (props) => {
     }
   };
 
+  const expenses = props.expenses;
+
   const filteredExpenses = expenses.filter(filterByDate);
+  let expensesContent = (
+    <p className="text-white font-bold text-3xl p-8">
+      No expenses found for selected year...
+    </p>
+  );
+
+  if (filteredExpenses.length > 0) {
+    expensesContent = filteredExpenses.map((expense) => (
+      <ExpenseItem
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      />
+    ));
+  }
 
   return (
     <div className="lg:max-w-[85%] p-4 bg-gradient-to-tl from-sky-300 my-8 mx-auto border-1 rounded-xl text-center shadow-lg">
       <ExpenseFilter onSelectYear={filterHandler} />
-      {filteredExpenses.length === 0 ? (
-        <p className="text-white font-bold text-3xl p-8">
-          No expenses found for selected year...
-        </p>
-      ) : (
-        filteredExpenses.map((expense) => (
-          <ExpenseItem
-            key={expense.id}
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        ))
-      )}
+      {expensesContent}
     </div>
   );
 };
